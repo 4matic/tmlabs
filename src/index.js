622dec8f17e4cca46d20b794435ed8653dfa3d31
4matic <maksim93@mail.ru>
2017-07-06 04:04:38 +0300

    Rollup config, es & umd building added.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ export default class TmLabs {
     console.log('Constants', Constants);
     console.log('Options', options);
   }
+  fetch = async (params) => {
+
+  }
+  setToken = (token) => {
+
+  }
 }
+//
+const fetch = (options) => {
+  const tmLabs = new TmLabs(options);
+  return tmLabs.fetch();
+};
+//
+export { fetch }
